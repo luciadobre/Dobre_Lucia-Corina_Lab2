7a43533c2e6e52c27f59843a8c2d5510b78d5392
Lucille <82943831+luciadobre@users.noreply.github.com>
2023-11-16 12:46:38 +0200

    category

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Dobre_Lucia_Corina_Lab2.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
+    }
+}
